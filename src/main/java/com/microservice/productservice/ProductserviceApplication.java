e7150a22b7db76648d58c5468a0ae726ddaa950b
user <tejaswini.vadje@neosoftmail.com>
2023-04-11 19:00:08 +0530

    productservice commit

--- src/main/java/com/microservice/productservice/ProductserviceApplication.java
+++ src/main/java/com/microservice/productservice/ProductserviceApplication.java
@@ -2,13 +2,12 @@ package com.microservice.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@ComponentScan("com.microservice.productservice")
-//@EnableJpaRepositories(basePackages ="ProdutRepository")
-
+@EnableDiscoveryClient
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
